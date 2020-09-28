e6804247cfffa101c8a8d003620aca5deca3f100
Nikola Malovic <nikola.malovic@outreach.io>
2020-09-28 20:25:48 +0200

    ContextParam has convenient ctor

--- src/context/ContextParam.ts
+++ src/context/ContextParam.ts
@@ -1,6 +1,30 @@
 import { AllContextKeys } from '../store/keys/AllContextKeys';
 
 export class ContextParam {
-    key: AllContextKeys;
-    value: string;
+    /**
+     * Parameter key
+     *
+     * @type {AllContextKeys}
+     * @memberof ContextParam
+     */
+    public key: AllContextKeys;
+
+    /**
+     * Parameter value
+     *
+     * @type {string}
+     * @memberof ContextParam
+     */
+    public value: string;
+
+    /**
+     * Creates an instance of ContextParam.
+     * @param {AllContextKeys} key
+     * @param {string} value
+     * @memberof ContextParam
+     */
+    constructor (key: AllContextKeys, value: string) {
+      this.key = key;
+      this.value = value;
+    }
 }
